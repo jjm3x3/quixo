95e951228c49936252284d1dae9d8ce7debe7bf6
Jacob M <jjm3333@gmail.com>
2016-11-06 09:59:10 -0800

    Start to compute possible states

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 	"strconv"
 )
 
+var (
+	theMoveList [][]int
+)
+
 func main() {
 
 	theBoard := newBoard(nil)
@@ -17,21 +21,42 @@ func main() {
 
 	moveList := getMoves()
 
+	nextState := make([]*board, 46)
+
+	fmt.Printf("how long is move list %v\n", len(moveList))
 	for i := 0; i < len(moveList); i++ {
 		theMove := moveList[i]
-		src, err := strconv.Atoi(theMove[0])
-		if err != nil {
-			log.Printf("problem casting src is not a number: %v\n", err)
-		}
-		dest, err := strconv.Atoi(theMove[1])
-		if err != nil {
-			log.Printf("problem casting src is not a number: %v\n", err)
-		}
-		err = theBoard.makeMove(src, dest)
-		if err != nil {
-
+		src := theMove[0]
+		dest := theMove[1]
+		err := theBoard.checkMove(src, dest)
+		if err == nil {
+			newState := copy(theBoard)
+			newState.makeMove(src, dest)
+			nextState[i] = newState
 		}
 	}
+
+	fmt.Printf("show me what you look like:\n %v\n", nextState)
+
+	newNextState := make([][]*board, len(nextState))
+	for i := 0; i < len(nextState); i++ {
+		startState := nextState[i]
+		newNextState[i] = make([]*board, len(moveList))
+		for j := 0; j < len(moveList); j++ {
+			if startState != nil {
+				err := startState.checkMove(moveList[j][0], moveList[j][1])
+				if err == nil {
+					newState := copy(startState)
+					newState.makeMove(moveList[j][0], moveList[j][1])
+					newNextState[i][j] = newState
+				}
+			}
+		}
+	}
+	fmt.Printf("lets see theseNext moves:\n %v\n", newNextState)
+
+	possibleMoves := howManyMoves(theBoard)
+	fmt.Printf("how many moves can I make?: %v\n", possibleMoves)
 
 	// getMoves()
 
@@ -39,7 +64,17 @@ func main() {
 
 }
 
-func getMoves() [][]string {
+func howManyMoves(board *board) int {
+	possibleMoves := 0
+	for i := 0; i < len(theMoveList); i++ {
+		if board.checkMove(theMoveList[i][0], theMoveList[i][1]) == nil {
+			possibleMoves++
+		}
+	}
+	return possibleMoves
+}
+
+func getMoves() [][]int {
 
 	file, err := os.Open("posibleMoves.csv")
 	defer file.Close()
@@ -54,11 +89,27 @@ func getMoves() [][]string {
 		log.Printf("Error opein move list: %v\n", err)
 	}
 
+	result := make([][]int, len(moves))
+
 	for i := 0; i < len(moves); i++ {
-		fmt.Printf("I see a move:  %v\n", moves[i])
+		theMove := moves[i]
+		src, err := strconv.Atoi(theMove[0])
+		if err != nil {
+			log.Printf("problem casting src is not a number: %v\n", err)
+		}
+		dest, err := strconv.Atoi(theMove[1])
+		if err != nil {
+			log.Printf("problem casting src is not a number: %v\n", err)
+		}
+		result[i] = []int{src, dest}
 	}
 
-	return moves
+	// for i := 0; i < len(moves); i++ {
+	// 	fmt.Printf("I see a move:  %v\n", moves[i])
+	// }
+
+	theMoveList = result // not sure which is preferable
+	return result
 }
 
 func playGame() {
