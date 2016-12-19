5c49f3b348be63f0711cc9e9f503c9ef556045f6
Jacob M <jjm3333@gmail.com>
2016-12-18 18:55:10 -0800

    Add a bots file with bot specifics and change default ai to NEURAL_NETWORK

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
 	"strconv"
 	"time"
@@ -40,7 +39,7 @@ func main() {
 			if theBoard.turn == true {
 				promptForMove(theBoard)
 			} else {
-				move := findNextMove(theBoard, BASIC_AI)
+				move := findNextMove(theBoard, NEURAL_NETWORK) //BASIC_AI)
 				theBoard.makeMove(move[0], move[1])
 				checkForWin(theBoard)
 			}
@@ -49,146 +48,6 @@ func main() {
 		playGame()
 	}
 
-}
-
-func findNextMove(theBoard *board, aiKind int) []int {
-
-	if aiKind == NEURAL_NETWORK {
-		return neuralNetwork(theBoard)
-	} else {
-		return basicAI(theBoard)
-	}
-
-}
-
-func neuralNetwork(theBoard *board) []int {
-	// numberOfMoves := howManyMoves(theBoard)
-	// numberOfPeices := howManyPeices(theBoard, theBoard.whoseTurn())
-
-	nextStates := getNextStates(theBoard)
-
-	// fmt.Printf("show me what you look like:\n %v\n", nextStates)
-
-	var (
-		bestMove  []int
-		bestScore = float64(50)
-	)
-	for i := 0; i < len(nextStates); i++ {
-		if nextStates[i] != nil {
-			newMoves := howManyMoves(nextStates[i])
-			numPeices := howManyPeices(nextStates[i], theBoard.whoseTurn())
-			outcome := determineOutcome(newMoves, numPeices)
-			log.Printf("yeilds result: %v vs current best: %v\n", outcome, bestScore)
-			if outcome < bestScore {
-				bestScore = outcome
-				bestMove = theMoveList[i]
-			}
-
-		}
-	}
-
-	return bestMove
-
-}
-
-type neuron struct {
-	w1,
-	w2 float64
-}
-
-func (n *neuron) compute(x, y float64) float64 {
-	return float64(x)*n.w1 + float64(y)*n.w2
-}
-
-func determineOutcome(x, y int) float64 {
-	log.Printf("%v , %v\n", x, y)
-	now := time.Now()
-	rand.Seed(int64(now.Nanosecond()))
-	// w1 := rand.Intn(45)
-	// w2 := rand.Intn(45)
-
-	// return x*w1 + y*w2
-
-	fx := float64(x)
-	fy := float64(y)
-	firstN := &neuron{0.5, 0.5}
-	firstN1 := &neuron{0.1, 0.2}
-	finalN := &neuron{0.7, 0.36}
-
-	return finalN.compute(firstN.compute(fx, fy), firstN1.compute(fx, fy))
-
-}
-
-func basicAI(theBoard *board) []int {
-	nextStates := getNextStates(theBoard)
-
-	// fmt.Printf("show me what you look like:\n %v\n", nextStates)
-
-	var (
-		bestMove   []int
-		mostMoves  int
-		mostPeices int
-	)
-	for i := 0; i < len(nextStates); i++ {
-		if nextStates[i] != nil {
-			newMoves := howManyMoves(nextStates[i])
-			// if newMoves > mostMoves {
-			// 	mostMoves = newMoves
-			// }
-
-			numPeices := howManyPeices(nextStates[i], theBoard.whoseTurn())
-			if numPeices > mostPeices {
-				mostPeices = numPeices
-				bestMove = theMoveList[i]
-			} else if numPeices == mostPeices && newMoves >= mostMoves {
-				bestMove = theMoveList[i]
-			}
-
-		}
-	}
-
-	return bestMove
-}
-
-func getNextStates(startState *board) []*board {
-
-	nextStates := make([]*board, len(theMoveList))
-
-	for i := 0; i < len(theMoveList); i++ {
-		theMove := theMoveList[i]
-		src := theMove[0]
-		dest := theMove[1]
-		err := startState.checkMove(src, dest)
-		if err == nil {
-			newState := copy(startState)
-			newState.makeMove(src, dest)
-			nextStates[i] = newState
-		}
-	}
-
-	return nextStates
-}
-
-func howManyMoves(board *board) int {
-	possibleMoves := 0
-	for i := 0; i < len(theMoveList); i++ {
-		if board.checkMove(theMoveList[i][0], theMoveList[i][1]) == nil {
-			possibleMoves++
-		}
-	}
-	return possibleMoves
-}
-
-func howManyPeices(board *board, player rune) int {
-	result := 0
-	for i := 0; i < len(board.grid); i++ {
-		for j := 0; j < len(board.grid[i]); j++ {
-			if board.getPosition(i, j) == player {
-				result++
-			}
-		}
-	}
-	return result
 }
 
 func getMoves() [][]int {
@@ -255,7 +114,7 @@ func playBots() {
 			theBoard.makeMove(move[0], move[1])
 			file1.WriteString(strconv.Itoa(move[0]) + "," + strconv.Itoa(move[1]) + "\n")
 		} else {
-			move := findNextMove(theBoard, BASIC_AI)
+			move := findNextMove(theBoard, NEURAL_NETWORK) //BASIC_AI)
 			theBoard.makeMove(move[0], move[1])
 			file2.WriteString(strconv.Itoa(move[0]) + "," + strconv.Itoa(move[1]) + "\n")
 		}
